3c96f7cea06feb76207c45934ae8f0f5438c4220
WaqasAliAnjum-ZeePalm <waqasali@zeepalm.com>
2023-11-19 13:29:30 +0500

    Added Splah Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 10));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
